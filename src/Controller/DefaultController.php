7055e379dc688e25ae4bc6e4966a0a5153812052
unknown <antoniogarciaillescas@gmail.com>
2019-04-26 14:30:40 +0200

    Añadidos comentarios y una pequeña modiciacion en el login AJAX

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -26,12 +26,11 @@ class DefaultController extends AbstractController
     /**
      * @Route("/logeoAjax", name="logeoAjax")
      */
-    public function logeoAjax(){
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        // $encriptedPass=password_hash($_POST['password'], PASSWORD_ARGON2I);
-        $usuario = $repository->findUserByEmailPass($_POST['email'], $_POST['password']);
-
-        if(isset($_POST['email'])) {
+    public function logeoAjax(){ //Obtenemos el usuario con el email y la contraseña que nos introduce el usuario en cada formulario
+        if(isset($_POST['email'])) {//Comprobamos si se han pasado ciertos datos por POST
+            $repository = $this->getDoctrine()->getRepository(User::class);
+            // $encriptedPass=password_hash($_POST['password'], PASSWORD_ARGON2I);
+            $usuario = $repository->findUserByEmailPass($_POST['email'], $_POST['password']);
             return $this->json(['username' => $usuario -> getEmail(), 'id' => $usuario -> getId(), 'nickname' => $usuario -> getNickname()]);
         } else {
             return $this->render('tableBoots.html.twig');
@@ -42,10 +41,10 @@ class DefaultController extends AbstractController
     /**
      * @Route("/crearPartida", name="crearPartida")
      */
-     public function crear_Partida(){
+     public function crear_Partida(){//Crearemos la partida introduciendole los datos que necesitamos y devolveremos un json para ir trabajando con el en la vista
         $repository = $this->getDoctrine()->getRepository(User::class);
         $repository2 = $this->getDoctrine()->getRepository(Dado::class);
-        $dado = $repository2 ->findOneById(1);
+        $dado = $repository2 ->findOneById(1);//Este es el dado de 6 caras que usaremos normalmente
 
         $random_number = mt_rand(1,9999999);
         $nueva_partida = new Partida();
@@ -53,13 +52,13 @@ class DefaultController extends AbstractController
         $nueva_partida->setId($random_number);
         $nueva_partida->setGanador("");
         $nueva_partida->setNumTurnos(0);
-        $nueva_partida->setDado($dado);
-        $date = new \DateTime('@'.strtotime('now'));
+        $nueva_partida->setDado($dado);//Guardamos el dato en la partida
+        $date = new \DateTime('@'.strtotime('now'));//Este es el formato que tendrá la fecha segun el tipo DATETIME
         $nueva_partida->setFecha($date);
         $entityManager->persist($nueva_partida);
         $entityManager->flush();
 
-        //Creamos los objetos Usuarios con los datos de los usuarios que se ha logueado para añadirlos a la partida en cuestion
+        //Creamos los objetos Usuarios con los datos de los usuarios que se ha logueado para añadirlos a la partida en cuestion, de manera que todos los jugadores que esten logueados serán los que jueguen
 
 
         // $repository2 = $this->getDoctrine()->getRepository(Partida::class);
@@ -69,11 +68,12 @@ class DefaultController extends AbstractController
         // die();
         foreach ($_POST['array_jugadores'] as $key => $value) {
             $usuario = $repository->findOneById($value['id']);
-            $nueva_partida->addJugadore($usuario);
+            $nueva_partida->addJugadore($usuario);//Añadimos cada jugador a la partida
             $entityManager->persist($nueva_partida);
             $entityManager->flush();
         }
 
+        //Devolvemos los valores que nos interesan de la partida
         return $this->json(['id_partida' => $nueva_partida->getId(), 'ganador' => $nueva_partida -> getGanador(), 'caras_dado' => $dado->getCaras()]);
         
 
